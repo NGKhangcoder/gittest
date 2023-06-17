a825d206e58385b2302bdb7d3ed2b8c4ebf2bbe9
khang <khangnguyen@onefin.vn>
2023-06-17 10:31:01 +0700

    done

--- js.js
+++ js.js
@@ -3,9 +3,13 @@ const url = "/utilities/ewallet/transaction/file-export?fromDate=&toDate=&excel=
 console.log(url.substring(url.indexOf("?") + 1).split("&"));
 123
 
+<<<<<<< HEAD
 
 asdasljkdkj
 
 
 i want another conflictasdas
 hello world
+=======
+hello world
+>>>>>>> 4431b54 (master)
