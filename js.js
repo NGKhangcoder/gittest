17527df6ac5785053e9fac59de75e445d033f04b
khang <khangnguyen@onefin.vn>
2023-06-17 16:18:22 +0700

    calling from B1

--- js.js
+++ js.js
@@ -5,7 +5,6 @@ console.log(url.substring(url.indexOf("?") + 1).split("&"));
 
 gitk
 
-<<<<<<< HEAD
 No Need Fixgp
 
 
@@ -13,6 +12,3 @@ No Need Fixgp
 i need Add new Feature to here
 
 12312
-=======
-No Need Fix
->>>>>>> 52636ec03f213251e95219da0a316886293cb0b6
